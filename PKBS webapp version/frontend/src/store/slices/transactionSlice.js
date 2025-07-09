1e78733d9eccd1b0cedaf23249fde8d5153c2462
Mike_ML/AI <oladimejialabim@gmail.com>
2025-07-09 16:26:00 +0000

    transactionSlic.js and dockerfile updated

---
+++ PKBS webapp version/frontend/src/store/slices/transactionSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const transactionSlice = createSlice({
+  name: 'transactions',
+  initialState: {},
+  reducers: {}
+});
+
+export default transactionSlice.reducer;
